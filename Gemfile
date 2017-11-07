4d94ba845d92c31a1ce1707f51584c9fa9aa73d3
japalacio0109 <japalacio0108@gmail.com>
2017-11-07 12:11:26 -0500

    listo para la busqueda por parámetros

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'mysql2', '0.4.9'
 gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
+# Use Kaminari as the pagination service
+gem 'kaminari'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,8 +3,34 @@ class LogsController < ApplicationController
   layout :resolve_layout
 
   def system
-    @t_result = AuDbau.all
+    @t_result = AuDbau.all.page params[:page]
     @t_field = ["Nº","Fecha/Hora","Tipo de Petición","Ruta de Acceso","IP - Nombre Equipo","Usuario","Parámetros","Datos de Proceso","Estado"]
+    # BETWEEN COALESCE(B.start_date,'1900-01-01') and COALESCE(B.end_date,CURRENT_TIMESTAMP)
+
+  end
+
+  def General
+    #code
+  end
+
+  def byDate
+    #code
+  end
+
+  def byUsua
+    #code
+  end
+
+  def byRequest
+    #code
+  end
+
+  def byCrud
+    #code
+  end
+
+  def byState
+    #code
   end
 
 end
